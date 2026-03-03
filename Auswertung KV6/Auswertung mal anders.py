799527468364a584b38a4833559daea6b64b35ff
echoisablub <leonielanz13@gmail.com>
2026-03-03 13:21:56 +0100

    many changes, doesnt work

--- Auswertung KV6/Auswertung mal anders.py
+++ Auswertung KV6/Auswertung mal anders.py
@@ -49,45 +49,61 @@ def compute_spectrum(y, delta_t):
 
 
 # Analyse der Laser-Daten:
-file_path = "Auswertung KV6\data\Filter 1\Data Channel 2.dat"
+file_path = "data/Filter 1/Data Channel 2.dat"
 positions, measurements = load_data(file_path)
 mean_data_laser = average_measurements(measurements)
+
+'''# Analyse für das Weißlicht- oder Jod-Interferogramm
+file_path_led = "Auswertung KV6/data/Filter 1/Data Channel 0.dat"
+positions_led, measurements_led = load_data(file_path_led)
+mean_data_led = average_measurements(measurements_led)'''
+
+# Dateipfade
+path_jod = "data/Iod 1/Data Channel 0.dat"
+path_ref = "data/Iod 1/Data Channel 1.dat"
+
+# Einlesen und Mittelwertbildung
+pos_jod, meas_jod = load_data(path_jod)
+mean_jod = average_measurements(meas_jod)
+
+pos_ref, meas_ref = load_data(path_ref)
+mean_ref = average_measurements(meas_ref)
+
 
 # Erste Interpolation der Laser-Daten
 x_new, y_new, f_interpol = interpolate(positions, mean_data_laser, factor=10)
 
-plt.plot(positions, mean_data_laser, '.', color='green', label="Datenpunkte")
+'''plt.plot(positions, mean_data_laser, '.', color='green', label="Datenpunkte")
 plt.plot(x_new, y_new, '-', color='red', label="Interpolation")
 plt.xlabel("Motorposition")
 plt.ylabel("Signal")
 plt.title("Laser-Daten: Interpolation vs. Originaldaten")
 plt.legend()
-plt.show()
-
+plt.show()'''
 
 
 # Bestimmung der Extrema
 peaks, troughs = find_extrema(y_new)
 maxima_positions = x_new[peaks]
 
-plt.plot(x_new, y_new, '-', color='red', label="Interpolation")
+'''plt.plot(x_new, y_new, '-', color='red', label="Interpolation")
 plt.plot(maxima_positions, f_interpol(maxima_positions), 'x', color='green', label="Maxima")
 plt.xlabel("Motorposition")
 plt.ylabel("Signal")
 plt.title("Laser-Daten: Interpolation mit Maxima")
 plt.legend()
-plt.show()
+plt.show()'''
 
 # Kalibrierfunktion bestimmen
 r, fit = linear_fit(maxima_positions)
 
-plt.plot(r, maxima_positions, 'x', color='orange', label="Maxima")
+'''plt.plot(r, maxima_positions, 'x', color='orange', label="Maxima")
 plt.plot(r, fit.intercept + fit.slope*r, '-', color='blue', label="Lineare Anpassung")
 plt.xlabel("Maxima")
 plt.ylabel("Motorposition")
 plt.title("Kalibrierfunktion: Lineare Anpassung der Maxima")
 plt.legend()
-plt.show()
+plt.show()'''
 
 P_soll = np.empty(len(maxima_positions), dtype=int)
 P_ist = maxima_positions
@@ -96,22 +112,50 @@ for i in range(len(maxima_positions)):
 delta = P_soll - P_ist
 delta_interpolate = UnivariateSpline(P_ist, delta, k=4)
 
-plt.plot(P_ist, delta, 'x', color='red', label="Daten")
+'''plt.plot(P_ist, delta, 'x', color='red', label="Daten")
 plt.plot(x_new, delta_interpolate(x_new), '-', color='blue', label="Interpolation der Korrektur")
 plt.xlabel("Motorposition")
 plt.ylabel("Korrektur (Soll - Ist)")
 plt.title("Korrektur der Motorpositionen")
 plt.legend()
-plt.show()
+plt.show()'''
 
-# Ortskorrektur
+'''# Erzeugen der korrigierten Ortsachse für die Weißlicht-Daten
+# x sind die ursprünglichen Motorpositionen
+x_korr_led = positions_led + delta_interpolate(positions_led)
+
+# Erzeugen einer äquidistanten Achse für die FFT
+x_korr_eq = np.linspace(x_korr_led.min(), x_korr_led.max(), len(x_korr_led))
+
+# Erneute Interpolation auf das äquidistante Gitter
+led_interpolate_func = UnivariateSpline(x_korr_led, mean_data_led, k=4)
+led_interferogram_korr = led_interpolate_func(x_korr_eq)'''
+
+# Korrektur der ursprünglichen Ortsachsen
+x_korr_jod = pos_jod + delta_interpolate(pos_jod)
+x_korr_ref = pos_ref + delta_interpolate(pos_ref)
+
+# Erzeugen eines gemeinsamen, äquidistanten Gitters für die FFT
+# Wichtig: Benutze für beide das exakt gleiche Gitter!
+x_min = max(x_korr_jod.min(), x_korr_ref.min())
+x_max = min(x_korr_jod.max(), x_korr_ref.max())
+x_eq = np.linspace(x_min, x_max, 10000)
+
+# Interpolation für Jod und Referenz
+spline_jod = UnivariateSpline(x_korr_jod, mean_jod, k=4)
+spline_ref = UnivariateSpline(x_korr_ref, mean_ref, k=4)
+
+y_eq_jod = spline_jod(x_eq)
+y_eq_ref = spline_ref(x_eq)
+
+'''# Ortskorrektur Laser
 x= np.linspace(-5000, 5000, 10000)
 x_korr = x + delta_interpolate(x)
 x_korr_eq = np.arange(min(x_korr), max(x_korr), (max(x_korr)-min(x_korr))/len(x_korr))
 
 # Neue Interpolation nach Korrektur
 laser_interpolate_func = UnivariateSpline(x_korr, mean_data_laser, k=4)
-laser_interpolate_korr = laser_interpolate_func(x_korr_eq)
+laser_interpolate_korr = laser_interpolate_func(x_korr_eq)'''
 
 #vergleich der Interpolation vor und nach Korrektur
 '''fig, (ax1, ax2) = plt.subplots(1, 2)
@@ -145,12 +189,23 @@ plt.title("Laser Spektrum")
 plt.show()
 '''
 
-delta_s = x_korr_eq*532*10**(-9)/fit.slope #: andere Gruppe
+'''delta_s = x_korr_eq*532*10**(-9)/fit.slope #: andere Gruppe
 delta_t = delta_s / c
 d=delta_t
-print(len(delta_s))
+print(len(delta_s))'''
 
-# FT 
+# Ort zu Zeit Trafo
+delta_s = x_eq * 532e-9 / fit.slope 
+delta_t_axis = delta_s / c 
+
+# FFT
+# compute_spectrum liefert Frequenzachse und Amplituden
+freq_axis, spectrum_jod = compute_spectrum(y_eq_jod, delta_t_axis[3] - delta_t_axis)
+_, spectrum_ref = compute_spectrum(y_eq_ref, delta_t_axis[3] - delta_t_axis)
+
+lambda_all = c / (freq_axis + 1e-12) * 1e9
+
+'''# FT 
 T_sample= ((max(d)-min(d))/len(d))*1e12 #Ps
 f_max=1/T_sample
 L=len(d)
@@ -161,14 +216,14 @@ W_imp=W/max(W) # normierung, weil?
 
 f_peak, _ = find_peaks(W_imp, prominence=0.15)
 print(f_peak)
-#von w den 6889 index -> w_postion von +peak = 563.6648002224845
+# von w den 6889 index -> w_postion von +peak = 563.6648002224845
 # mittelpunkt peak = 0.14915713157506616
 # w_postion von -peak = -563.3664859593342
-#print(w[5000])
+# print(w[5000])
 
-f_mittelwert = 563.515643091*10**12
-lambda_f =c/f_mittelwert
-print(lambda_f)
+# f_mittelwert_laser = 563.515643091*10**12
+# lambda_f =c/f_mittelwert_laser
+# print(lambda_f)
 # =5.320037902684942e-07 =532nm yaaay
 
 plt.plot(w,W_imp, color='purple')
@@ -189,5 +244,43 @@ plt.xlabel("Wellenlänge [nm]")
 plt.ylabel("Amplitude")
 plt.title("Laser Spektrum, Wellenlänge")
 plt.show()
-
+'''
 #Todo: optische dichte und so
+
+#Um subtile Details in den gemessenen Spektren sichtbar zu machen, ist es sinnvoll, die Daten mit
+#einem Referenzspektrum zu korrigieren. Dazu wird typischerweise die optische Dichte OD =
+#−log(I/I0) oder aber auch die differentielle optische Transmission (I −I0)/I0 berechnet. I0
+#bezieht sich dabei auf das Referenzspektrum. Berechnen Sie wahlweise die OD oder DOT aus
+#den Spektren der Jod- und Referenzprobe
+
+# 1. Berechnung der Optischen Dichte (OD)
+# Formel: OD = -log10(I / I0)
+# Wir nutzen np.log10, da OD im physikalischen Kontext meist dekadisch ist.
+# Ein kleines epsilon verhindert Division durch Null.
+epsilon = 1e-12
+od = -np.log10((spectrum_jod + epsilon) / (spectrum_ref + epsilon))
+
+# 2. Berechnung der Differentiellen Optischen Transmission (DOT)
+# Formel: DOT = (I - I0) / I0
+dot = (spectrum_jod - spectrum_ref) / (spectrum_ref + epsilon)
+
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 10), sharex=True)
+# Darstellung der Optischen Dichte (OD)
+ax1.plot(lambda_all, od, color='darkred', label='Optische Dichte (OD)')
+ax1.set_ylabel("OD")
+ax1.set_title("Absorptionsspektrum der Jod-Probe (OD)")
+ax1.grid(True)
+ax1.legend()
+# Darstellung der Differentiellen Transmission (DOT)
+ax2.plot(lambda_all, dot, color='blue', label='Diff. Transmission (DOT)')
+ax2.set_xlabel("Wellenlänge [nm]")
+ax2.set_ylabel("DOT")
+ax2.set_title("Spektrum: Differentielle Transmission (DOT)")
+ax2.grid(True)
+ax2.legend()
+# Bereich auf den interessanten Teil begrenzen (z.B. 450nm bis 650nm für Jod)
+plt.xlim(450, 700) 
+plt.tight_layout()
+plt.show()
+
