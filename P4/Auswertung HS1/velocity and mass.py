2a4ec539c2c0b61aa1c2fe2db5c9ea183183adc9
echoisablub <leonielanz13@gmail.com>
2026-09-08 11:49:55 +0200

    plots für milky way 21 cm linie stuff

---
+++ P4/Auswertung HS1/velocity and mass.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+filepath="Auswertung HS1/calibrated 21cm/cm21_tab.txt"
+data = np.loadtxt(filepath, skiprows=1)
+l = data[:,0]
+R = data[:,1]
+R_err = data[:,2]
+v_max = data[:,3]
+v_max_err = data[:,4]
+
+R_0 = 8.5 #kpc
+omeg_0 = 220e-16/R_0 #s-1
+M_sun = 1.98e30 #kg
+
+v_R = []
+v_R_err = []
+M_R = []
+M_R_err = []
+
+for i in range(len(l)):
+    v_R_i = v_max[i] + omeg_0 * R[i] #R = R_0 * np.sin(l) R=[kpc] v_R=[kms-1]
+    v_R_err_i = np.sqrt(v_max_err[i]**2 + R_err[i]**2)
+    M_R_i = 0.234e10 * (v_R_i / 100)**2 * R[i] #in Sonnenmassen (M_sun = 1.98e30 kg)
+    M_R_err_i = np.sqrt(2*234e6 * v_R_i * R[i] * v_R_err_i**2 + 0.234e10 * (v_R_i / 100)**2 * R_err[i]**2)
+
+    v_R.append(v_R_i)
+    v_R_err.append(v_R_err_i)
+    M_R.append(M_R_i)
+    M_R_err.append(M_R_err_i)
+
+v_R = np.array(v_R)
+v_R_err = np.array(v_R_err)
+M_R = np.array(M_R)
+M_R_err = np.array(M_R_err)
+
+data_new = np.column_stack((data, v_R, v_R_err, M_R, M_R_err))
+filepath_newdata="Auswertung HS1/calibrated 21cm/cm21_tab_newdata.txt"
+np.savetxt(
+    filepath_newdata,
+    data_new,
+    header="l[deg] R[kpc] R_err v_max[kms-1] v_max_err v_R[kms-1] M_R[M_sun], M_R_err",
+    comments="",
+    fmt=["%.0f", "%.2f", "%.2f", "%.2f", "%.2f", "%.2f", "%.2f", "%.2f", "%.2f"]
+)
+
+plt.errorbar(
+    R,
+    M_R,
+    yerr=M_R_err,
+    fmt="x",
+    capsize=3,
+    label=r"$M_R$ mit Fehlerbalken"
+)
+
+plt.xlabel(r"$R$ [kpc]")
+plt.ylabel(r"$M_R$ [solar masses]")
+plt.title("Enclosed mass of the Milky Way (in solar masses) at each distance R")
+plt.grid(True)
+plt.legend()
+plt.tight_layout()
+plt.show()
