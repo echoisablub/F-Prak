da7afeb22cea2071540f86075f119aa8a236376e
voidofsea <emma.kalogerakis@studium.uni-hamburg.de>
2026-09-13 12:58:36 +0200

    prot update

--- P6/Daten/Analysis and Interpretation/Ref Spectrum/ref_spectrum.py
+++ P6/Daten/Analysis and Interpretation/Ref Spectrum/ref_spectrum.py
@@ -3,13 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 plt.rcParams.update({
-    "font.size": 14,
-    "axes.titlesize": 14,
-    "axes.labelsize": 14,
-    "xtick.labelsize": 14,
-    "ytick.labelsize": 14,
-    "legend.fontsize": 14,
-    "legend.title_fontsize": 14,
+    "font.size": 20,
+    "axes.titlesize": 20,
+    "axes.labelsize": 20,
+    "xtick.labelsize": 20,
+    "ytick.labelsize": 20,
+    "legend.fontsize": 20,
+    "legend.title_fontsize": 20,
 })
 
 path = "Daten/Analysis and Interpretation/Ref Spectrum/Reference_Data.csv"
